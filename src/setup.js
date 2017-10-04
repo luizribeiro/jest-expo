121d89f8668abf0092d73e7eb7fbfda188d358ad
Luiz Ribeiro <luizribeiro@gmail.com>
2017-10-03 23:48:31 -0300

    Setup mocks for Expo.Svg

--- src/setup.js
+++ src/setup.js
@@ -120,3 +120,30 @@ Object.defineProperty(ReactNative, 'Modal', {
   enumerable: true,
   get: () => Modal,
 });
+
+/* mock Expo.Svg */
+const React = require.requireActual('react');
+
+function createSvgComponentMock(name, actualComponent) {
+  class SvgComponentMock extends React.Component {
+    render() {
+      return React.createElement(name, this.props, this.props.children);
+    }
+  }
+  SvgComponentMock.displayName = actualComponent.displayName;
+  SvgComponentMock.propTypes = actualComponent.propTypes;
+  return SvgComponentMock;
+}
+
+const Svg = createSvgComponentMock('Svg', Expo.Svg);
+const excludedKeys = ['displayName', 'propTypes', 'defaultProps'];
+const componentsToMock = Object.keys(Expo.Svg)
+  .filter(key => !excludedKeys.includes(key));
+for (let name of componentsToMock) {
+  Svg[name] = createSvgComponentMock(name, Expo.Svg[name]);
+}
+
+Object.defineProperty(Expo, 'Svg', {
+  enumerable: true,
+  get: () => Svg,
+});
